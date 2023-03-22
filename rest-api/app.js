cd62d6ed7a140cbd50cb763d63dbb7bce6501135
008adarshshukla <shuklaadarsh1932@gmail.com>
2023-03-22 13:58:24 +0530

    error handling added

--- rest-api/app.js
+++ rest-api/app.js
@@ -12,4 +12,21 @@ app.use('/products', porductRoutes)
 //prividing routes for http://localhost/orders
 app.use("/orders", ordersRoutes)
 
+//if all of the above mentioned routes do not exist we throw an error.
+app.use((req, res, next) => {
+    const error = Error('Not found')
+    error.status = 404
+    next(error)
+})
+
+//handling errors thrown by routes.
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+     res.json({
+        error: {
+            message: error.message
+        }
+     })
+})
+
 module.exports = app 
